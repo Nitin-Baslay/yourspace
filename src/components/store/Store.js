d742371e0cf31a4d3b79d1934d42c7854a63d85e
Nitin Kumar <113196521+Nitin-Baslay@users.noreply.github.com>
2023-07-10 15:49:32 +0530

    Delete src/components directory

--- src/components/store/Store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import CounterSlice from "./CounterSlice";
-
-const Store = configureStore({
-  reducer: CounterSlice,
-});
-export default Store;
